377a73400b11230d86d4d7c8bdd793b2ffcba098
Topsy-Goo <topsygoo@yandex.ru>
2022-05-15 16:23:22 +0300

    Отложено.
    Метеостанция выглядит избыточной, учитывая, что погоду можно узнать из браузера.

--- smart-weather-station/src/main/java/ru/gb/smarthome/weatherstation/DeviceClientWeatherStation.java
+++ smart-weather-station/src/main/java/ru/gb/smarthome/weatherstation/DeviceClientWeatherStation.java
@@ -2,19 +2,57 @@ package ru.gb.smarthome.weatherstation;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import ru.gb.smarthome.common.smart.IConsolReader;
+import ru.gb.smarthome.common.smart.structures.Abilities;
 import ru.gb.smarthome.empty.client.DeviceClientEmpty;
+
+import javax.annotation.PostConstruct;
+
+import java.util.concurrent.Executors;
+
+import static ru.gb.smarthome.common.FactoryCommon.CANNOT_SLEEP;
+import static ru.gb.smarthome.common.smart.enums.DeviceTypes.FRIDGE;
+import static ru.gb.smarthome.common.smart.enums.DeviceTypes.METEO_STATION;
 
 @Component
 @Scope ("prototype")
 public class DeviceClientWeatherStation extends DeviceClientEmpty
 {
-    private final PropertyManagerWeatherStation propMan;
+    //private final PropertyManagerWeatherStation propMan;
 
     public DeviceClientWeatherStation (PropertyManagerWeatherStation pmws) {
         super(pmws);
-        propMan = pmws;
     }
 
-    @Override public void run () {
+    @PostConstruct
+    public void init ()
+    {
+        abilities = new Abilities(
+                METEO_STATION,
+                propMan.getName(),
+                propMan.getUuid(),
+                CANNOT_SLEEP)
+                .setTasks (propMan.getAvailableTasks())
+                .setSensors (propMan.getAvailableSensors())
+                .setSlaveTypes (propMan.slaveTypes())
+                ;
+        sensorsNumber = abilities.getSensors().size();
+
+        taskExecutorService = Executors.newSingleThreadExecutor (r->{
+                        Thread t = new Thread (r);
+                        t.setDaemon (true);
+                        return t;});
+        //if (DEBUG) {
+            Thread threadConsole = new Thread (()->IConsolReader.runConsole (this));
+            threadConsole.setDaemon(true);
+            threadConsole.start();
+        //}
+        taskExecutorService.execute (this::workHard);
+    }
+
+/** Run-метод потока, ответственного за полуение сведений о погоде из метеослужбы. */
+    private void workHard ()
+    {
+        //TODO: Нам здесь понадобятся координаты, которые юзер введёт на фронте.
     }
 }
--- smart-weather-station/src/main/java/ru/gb/smarthome/weatherstation/PropertyManagerWeatherStation.java
+++ smart-weather-station/src/main/java/ru/gb/smarthome/weatherstation/PropertyManagerWeatherStation.java
@@ -2,27 +2,23 @@ package ru.gb.smarthome.weatherstation;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import ru.gb.smarthome.common.smart.structures.Task;
 import ru.gb.smarthome.empty.PropertyManagerEmpty;
 
 import javax.annotation.PostConstruct;
-import java.util.Set;
 import java.util.UUID;
+
+import static ru.gb.smarthome.common.FactoryCommon.uuidFromString;
 
 @Component ("fridge_propman")
 @Scope ("singleton")
 public class PropertyManagerWeatherStation extends PropertyManagerEmpty
 {
     private final String name = "Метео-Р 216 (ТА)";
-    private final UUID   uuid = UUID.fromString ("55a17017-508f-454f-8015-a4cc62403671");
-    private final Set<Task> tasks = emptyTaskList();
+    private final UUID   uuid = uuidFromString ("55a17017-508f-454f-8015-a4cc62403671");
 
+    @PostConstruct
+    @Override public void init() {}
 
-    @PostConstruct public void init() {}
-
-    public void shutdown() {}
-
-    @Override public UUID getUuid ()                { return uuid; }
-    @Override public String getName ()              { return name; }
-    @Override public Set<Task> getAvailableTasks () {  return tasks;  }
+    @Override public UUID getUuid ()   { return uuid; }
+    @Override public String getName () { return name; }
 }
